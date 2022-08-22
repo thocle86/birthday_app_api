b109b8a9d8e8dd7322d10483bf064a001686a9dc
Thomas Clement <thocle86@gmail.com>
2022-08-22 11:46:13 +0200

    Initialisation du projet

---
+++ src/test/java/fr/cefim/birthdayapp/BirthdayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cefim.birthdayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
